074100ffd306288c2363bbd5a7345a1e2f2efbae
Grish Gautam <grishgautam03@gmail.com>
2024-10-04 19:38:36 +0530

    next

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text("Notifications"),
+    ),
+    body: Text('Home Page'),
+  );
+}
