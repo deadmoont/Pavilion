60c978e175c155e977b262e86bdfc64c2c235b57
Naitik Jain <naitikjain2005@gmail.com>
2024-10-17 19:33:56 +0530

    commit 11 naitik : hello every one

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -1,44 +1,43 @@
-import 'package:flutter/material.dart';
-import '../models/event_model.dart';
-import '../widgets/event_card.dart';
-
-
-class EventScreen extends StatelessWidget {
-  final List<Event> events = [
-    Event(title: 'GRAFFITI'),
-    Event(title: 'FACE PAINTING'),
-    Event(title: 'SCHIZZ'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(
-          'Nirmiti',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: events.length,
-        itemBuilder: (context, index) {
-          return EventCard(event: events[index]);
-        },
-      ),
-      
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import '../models/event_model.dart';
+//
+//
+// class EventScreen extends StatelessWidget {
+//   final List<Event> events = [
+//     Event(title: 'GRAFFITI'),
+//     Event(title: 'FACE PAINTING'),
+//     Event(title: 'SCHIZZ'),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.black,
+//         elevation: 0,
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back, color: Colors.white),
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//         title: Text(
+//           'Nirmiti',
+//           style: TextStyle(
+//             color: Colors.white,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//         centerTitle: true,
+//       ),
+//       body: ListView.builder(
+//         padding: const EdgeInsets.all(16.0),
+//         itemCount: events.length,
+//         itemBuilder: (context, index) {
+//           return EventCard(event: events[index]);
+//         },
+//       ),
+//
+//     );
+//   }
+// }
