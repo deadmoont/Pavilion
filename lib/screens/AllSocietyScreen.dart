fcc3935ec6020c4ca1f50f2b5f96d094401f8e45
Grish Gautam <grishgautam03@gmail.com>
2024-10-20 21:19:30 +0530

    ....

--- lib/screens/AllSocietyScreen.dart
+++ lib/screens/AllSocietyScreen.dart
@@ -4,14 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:pavilion/database/Apis.dart';
 import 'package:pavilion/screens/event_screen.dart';
 
-/*
-This page shows the list of all socity which has 6 block 
-which indeed has list of event of the partiual socity 
-which indeed when clickeck shows  teh event details page 
-
-
-EVENTS PAGE IT IS
-*/
 class AllSocietyScreen extends StatefulWidget {
   @override
   State<AllSocietyScreen> createState() => _AllSocietyScreenState();
@@ -22,32 +14,32 @@ class _AllSocietyScreenState extends State<AllSocietyScreen> {
     {
       "name": "AMS",
       "description":
-          "A society dedicated to capturing moments through the lens, focusing on photography and videography."
+      "A society dedicated to capturing moments through the lens, focusing on photography and videography."
     },
     {
       "name": "GeneticX",
       "description":
-          "A dynamic dance group that explores various dance forms and promotes self-expression through movement."
+      "A dynamic dance group that explores various dance forms and promotes self-expression through movement."
     },
     {
       "name": "Nirmiti",
       "description":
-          "A creative club for fine arts enthusiasts, nurturing skills in painting, sketching, and other artistic expressions."
+      "A creative club for fine arts enthusiasts, nurturing skills in painting, sketching, and other artistic expressions."
     },
     {
       "name": "Rangtarangini",
       "description":
-          "A drama society that brings stories to life through acting, theater performances, and stage plays."
+      "A drama society that brings stories to life through acting, theater performances, and stage plays."
     },
     {
       "name": "Virtuosi",
       "description":
-          "A music group that celebrates the art of sound, providing a platform for singers, instrumentalists, and music lovers."
+      "A music group that celebrates the art of sound, providing a platform for singers, instrumentalists, and music lovers."
     },
     {
       "name": "Sarasva",
       "description":
-          "A literary club that encourages reading, writing, debates, and discussions, fostering a love for literature."
+      "A literary club that encourages reading, writing, debates, and discussions, fostering a love for literature."
     },
   ];
 
@@ -62,6 +54,13 @@ class _AllSocietyScreenState extends State<AllSocietyScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Get device width and height
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    // Determine the childAspectRatio dynamically based on screen width and height
+    double childAspectRatio = screenWidth / (screenHeight / 1.5); // Adjust as needed
+
     return Scaffold(
       backgroundColor: const Color(0xFF121212),
       appBar: AppBar(
@@ -70,7 +69,7 @@ class _AllSocietyScreenState extends State<AllSocietyScreen> {
           "Events",
           style: GoogleFonts.dmSans(
             textStyle: const TextStyle(
-              fontSize: 28.0, // Increased font size
+              fontSize: 28.0,
               color: Colors.white,
               fontWeight: FontWeight.bold,
             ),
@@ -80,94 +79,90 @@ class _AllSocietyScreenState extends State<AllSocietyScreen> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // 2 items per row
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: screenWidth > 600 ? 3 : 2, // Adjust columns based on screen width
             crossAxisSpacing: 10.0,
             mainAxisSpacing: 10.0,
-            childAspectRatio: 0.8, // Adjusted for a better aspect ratio
+            childAspectRatio: childAspectRatio, // Dynamically adjust aspect ratio
           ),
           itemCount: societyList.length,
           itemBuilder: (context, index) {
             return InkWell(
               onTap: () {
-                // Navigate to the EventScreen when the container is tapped
+                // Navigate to the EventScreen when tapped
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => EventScreen(
                       list: APIs.societyeventslist[index],
                       item: societyList[index]["name"]!,
-                    ), // Replace with your EventScreen widget
+                    ),
                   ),
                 );
               },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 4.0, vertical: 2), // Added horizontal padding
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16.0),
-                    color: Colors.black,
-                    border: Border.all(color: Colors.white, width: 2),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: const BorderRadius.vertical(
-                          top: Radius.circular(16.0),
-                        ),
-                        child: CachedNetworkImage(
-                          imageUrl: imageUrls[index],
-                          height: 85,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) => Container(
-                            height: 100,
-                            color: Colors.grey[200],
-                            child: const Center(
-                              child: CircularProgressIndicator(),
-                            ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  color: Colors.black,
+                  border: Border.all(color: Colors.white, width: 2),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(16.0),
+                      ),
+                      child: CachedNetworkImage(
+                        imageUrl: imageUrls[index],
+                        height: screenHeight * 0.12, // Adjust height based on screen height
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) => Container(
+                          height: screenHeight * 0.12,
+                          color: Colors.grey[200],
+                          child: const Center(
+                            child: CircularProgressIndicator(),
                           ),
-                          errorWidget: (context, url, error) => Container(
-                            height: 100,
-                            color: Colors.grey[200],
-                            child: const Icon(
-                              Icons.error,
-                              color: Colors.red,
-                            ),
+                        ),
+                        errorWidget: (context, url, error) => Container(
+                          height: screenHeight * 0.12,
+                          color: Colors.grey[200],
+                          child: const Icon(
+                            Icons.error,
+                            color: Colors.red,
                           ),
                         ),
                       ),
-                      const SizedBox(height: 5),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          societyList[index]["name"]!,
-                          style: GoogleFonts.dmSans(
-                            textStyle: const TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          societyList[index]["description"]!,
-                          style: GoogleFonts.dmSans(
-                            textStyle: const TextStyle(
-                              fontSize: 12.0,
-                              color: Colors.white70,
-                            ),
+                    ),
+                    const SizedBox(height: 5),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        societyList[index]["name"]!,
+                        style: GoogleFonts.dmSans(
+                          textStyle: TextStyle(
+                            fontSize: screenWidth * 0.045, // Responsive text size
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
                         ),
+                        textAlign: TextAlign.center,
                       ),
-                    ],
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        societyList[index]["description"]!,
+                        style: GoogleFonts.dmSans(
+                          textStyle: TextStyle(
+                            fontSize: screenWidth * 0.03, // Responsive text size
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             );
